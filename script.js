a98d0db306a3edbaa3ff0246f8060d4aa28cfaa3
a13579pppo <a91919191918182837@gmail.com>
2025-05-17 13:14:33 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function connectWallet() {
+  const manifestUrl = 'https://yourusername.github.io/ZynBet-Site/tonconnect-manifest.json';
+  alert('Connecting to Tonkeeper using: ' + manifestUrl);
+  // TonConnect SDK logic would be placed here.
+}
