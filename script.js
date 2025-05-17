8513b6dac73a9c81543951ddc7cb01c444bea6df
a13579pppo <a91919191918182837@gmail.com>
2025-05-17 13:00:32 +0330

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function navigate(section) {
-  document.querySelectorAll('.tab').forEach(tab => tab.classList.remove('active'));
-  document.getElementById(section).classList.add('active');
-}
-
-// اتصال به Tonkeeper واقعی در آینده اینجا انجام میشه.
